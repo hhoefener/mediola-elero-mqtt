40b8ed98c3833a922a7d7f43c58905866f282383
hhoefener <11026728+hhoefener@users.noreply.github.com>
2025-03-03 12:03:43 +0100

    replace mqtt dummy with real mqtt

--- main.py
+++ main.py
@@ -7,11 +7,8 @@ from utils import BlindCommand, load_config
 def main():
     config = load_config()
     mediola = Mediola(config.mediola.host, config.mediola.password, config.mediola.follow_up_time, debug=True)
-    mqtt = MQTTdummy()
-
-    for blind in config.blinds:
-        mediola.move_blind(blind, BlindCommand.OPEN, mqtt)
-
+    mqtt = MQTT(config.mqtt, config.blinds, move_blind_callback=mediola.move_blind, debug=True)
+    mqtt.loop_forever()
 
 if __name__ == '__main__':
     main()
