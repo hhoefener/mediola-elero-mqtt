e11eb5798ee91e8f187c41ed79567101824ab9ab
hhoefener <11026728+hhoefener@users.noreply.github.com>
2025-03-03 13:37:57 +0100

    switch back to loop_forever

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ def main():
     config = load_config()
     mediola = Mediola(config.mediola.host, config.mediola.password, config.mediola.follow_up_time, debug=True)
     mqtt = MQTT(config.mqtt, config.blinds, move_blind_callback=mediola.move_blind, debug=True)
+    
     mqtt.loop_forever()
 
 if __name__ == '__main__':
